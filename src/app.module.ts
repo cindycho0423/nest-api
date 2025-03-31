fa00d4395ae19bb576bebd9da5f3863394d42072
cindyCho <hjcho2303@naver.com>
2025-03-31 19:04:10 +0900

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // Decorator는 클래스에 함수 기능을 추가할 수 있다.
 // 클래스 위의 함수이고, 클래스를 위해 움직인다.
 // 데코레이터는 꾸며주는 함수나 클래스랑 붙어있어야 한다.
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
 // AppModule은 루트 모듈과 같다.
