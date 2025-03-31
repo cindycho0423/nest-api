f87a7b738f1fd9dd7249f50210a28b5cc6fd2ecd
cindyCho <hjcho2303@naver.com>
2025-03-31 19:08:26 +0900

    validation pipe 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // main에서 시작한다. 하나의 모듈에서 어플리케이션을 생성한다.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
