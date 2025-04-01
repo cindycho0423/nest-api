6f58144a2f372ab346c8472b938987b5dee77465
cindyCho <hjcho2303@naver.com>
2025-04-01 22:50:48 +0900

    amazone test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -23,13 +23,6 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('GET', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Welcome to my Movie API');
-  });
-
   describe('/movies', () => {
     it('GET', () => {
       return request(app.getHttpServer()) //
