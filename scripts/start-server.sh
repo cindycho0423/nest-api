9e54fef2fb1259025f145fb084736a318d0eecef
cindyCho <hjcho2303@naver.com>
2025-04-04 23:00:51 +0900

    docker

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram-server
-npm i
-pm2 kill
-pm2 start dist/main.js --name "backend-server"
+docker stop nest-api || true
+docker rm nest-api || true
+docker pull 977099027173.dkr.ecr.ap-northeast-2.amazonaws.com/nest-api:latest
+docker run -d --name nest-api -p 3000:3000 977099027173.dkr.ecr.ap-northeast-2.amazonaws.com/nest-api:latest
 echo "--------------- 서버 배포 끝 -----------------"
